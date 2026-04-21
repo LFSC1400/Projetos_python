77abd0eadfe023d77b3e0e0d5111567ec52201a9
LFSC1400 <silveiracostal0@gmail.com>
2026-04-20 23:33:17 -0300

    Atualização da calculadora

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -17,13 +17,8 @@ if operação == "/":
         print(f"O resultado da divisão é: {resultado}")
 
 if operação == ".":
-    if n1 == 0:
-        print("Quando um numero é multiplicado por 0, o resutado final é 0.")
-    if n2 == 0:
-        print("Quando um numero é multiplicado por 0, o resutado final é 0.")    
-    else:
-        resultado = n1 . n2
-        print(f"O resultado da multiplicação é: {resultado}")
+    resultado = n1 . n2
+    print(f"O resultado da multiplicação é: {resultado}")
 
 if operação == "+":
     resultado = n1 + n2
